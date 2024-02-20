fd56e490f5febbb1b0ca9056fe06d2f596c65df2
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2024-02-20 18:20:19 +0800

    messed with collision

--- window.c
+++ window.c
@@ -1,17 +1,33 @@
 #include "raylib.h"
+#include <stdlib.h>
 
 int main(void) {
 	
-	const int WIDTH=800;
-	const int HEIGHT=450;
+	const int WinWIDTH=800;
+	const int WinHEIGHT=600;
 
-	InitWindow(WIDTH,HEIGHT, "raylib - basic window");
+	InitWindow(WinWIDTH,WinHEIGHT, "Tetris");
+	SetTargetFPS(120);
+	
+	Rectangle recA = {50, GetScreenHeight()/2.0f - 100, 200, 100 };
+	int recASpeedX = 2;
+
+	int screenUpperLimit = 40;
+	bool pause = false;
+//	bool collision = false;
+
 
 	while (!WindowShouldClose()) 
 	{
+		if (!pause) recA.x += recASpeedX;
+		if (((recA.x + recA.width) >= GetScreenWidth()) || (recA.x <= 0)) recASpeedX *= -1;
+		if (IsKeyPressed(KEY_SPACE)) pause = !pause;
+
+
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
-			DrawRectangle( 200, 200, 200, 100, PURPLE);
+			DrawRectangleRec(recA, GREEN);
+			DrawFPS(5, 5);
 		EndDrawing();
 	}
 
